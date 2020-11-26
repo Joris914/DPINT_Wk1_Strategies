d25180d7445ec3e5222c31776380ac65a822dc2d
Joris Koevoets <63403671+Joris914@users.noreply.github.com>
2020-11-26 04:32:43 +0100

    Removed string "Numerical" from ViewModel

--- ValueConverterViewModel.cs
+++ ValueConverterViewModel.cs
@@ -2,6 +2,7 @@
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows.Input;
 
 namespace DPINT_Wk1_Strategies
@@ -94,10 +95,11 @@ namespace DPINT_Wk1_Strategies
 				ConverterNames.Add(name);
 			}
 
-			FromConverter = Factory.getConverter("Numerical");
-			ToConverter = Factory.getConverter("Numerical");
-			FromConverterName = "Numerical";
-			ToConverterName = "Numerical";
+			FromConverterName = Factory.ConverterNames.First();
+			ToConverterName = Factory.ConverterNames.First();
+
+			FromConverter = Factory.getConverter(FromConverterName);
+			ToConverter = Factory.getConverter(ToConverterName);
 
 			FromText = "0";
 			ToText = "0";
